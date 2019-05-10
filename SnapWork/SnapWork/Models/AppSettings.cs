628f8e2635c2e2eb378c24b53ab5b6cbdf6be193
Илья Безуглый <Terorist1999_987654321>
2019-05-10 22:22:00 +0300

    Добавьте файлы проекта.

---
+++ SnapWork/SnapWork/Models/AppSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnapWork.Models
+{
+    public static class AppSettings
+    {
+        public static bool IsLogged = false;
+    }
+}
---
+++ SnapWork/SnapWork/Views/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace SnapWork.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            MasterBehavior = MasterBehavior.Popover;
+            Detail = new NavigationPage(new Search());
+        }
+
+
+        private void AddNew_clicked(object o, EventArgs e)
+        {
+            Detail = new NavigationPage(new AddNew());
+            IsPresented = false;
+        }
+
+        private void Search_clicked(object o, EventArgs e)
+        {
+            Detail = new NavigationPage(new Search());
+            IsPresented = false;
+        }
+
+        private void Messages_clicked(object o, EventArgs e)
+        {
+            Detail = new NavigationPage(new Messages());
+            IsPresented = false;
+        }
+
+        private void Settings_clicked(object o, EventArgs e)
+        {
+            Detail = new NavigationPage(new Settings());
+            IsPresented = false;
+        }
+
+        private void Vacantions_clicked(object o, EventArgs e)
+        {
+            Detail = new NavigationPage(new Vacantions());
+            IsPresented = false;
+        }
+        
+        private void Favorite_clicked(object o, EventArgs e)
+        {
+            Detail = new NavigationPage(new Favorite());
+            IsPresented = false;
+        }
+    }
+}
